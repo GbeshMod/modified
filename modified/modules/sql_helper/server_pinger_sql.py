6b11c80a21feedd46d08247051100030bbb3ccb2
Gbesh <63529893+GbeshMod@users.noreply.github.com>
2021-02-10 14:41:24 +0000

    Add files via upload

---
+++ modified/modules/sql_helper/server_pinger_sql.py
@@ -0,0 +1,43 @@
+
+from sqlalchemy import Column, UnicodeText
+
+from modified.modules.sql_helper import BASE, SESSION
+
+
+class Serverpinger(BASE):
+    __tablename__ = "serverpinger"
+    url = Column(UnicodeText, primary_key=True)
+
+    def __init__(self, url):
+        self.url = url
+
+
+Serverpinger.__table__.create(checkfirst=True)
+
+
+def add_ping(url):
+    pinger = Serverpinger(url)
+    SESSION.add(pinger)
+    SESSION.commit()
+
+
+def rmping(url):
+    rmpinger = SESSION.query(Serverpinger).get(url)
+    if rmpinger:
+        SESSION.delete(rmpinger)
+        SESSION.commit()
+
+
+def get_all_url():
+    stark = SESSION.query(Serverpinger).all()
+    SESSION.close()
+    return stark
+
+
+def is_ping_indb(url):
+    try:
+        return SESSION.query(Serverpinger).filter(Serverpinger.url == url).one()
+    except:
+        return None
+    finally:
+        SESSION.close()
