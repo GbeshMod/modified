ae78887bea3796ae23f7ca8c6db623ce32fb102d
Gbesh <63529893+GbeshMod@users.noreply.github.com>
2021-02-16 10:09:57 +0000

    Add files via upload

---
+++ python CONFIG.py
@@ -0,0 +1,103 @@
+# Hola amigo...  Please take 📝 NOTE you only have to edit line 50 to 124.. 
+# we also have some argument in line 11 to 45  Because of some Plugins from others user bot.
+# I won't Develop or upgrade the modified bot    it all yours😋
+
+
+from modified.Configs.ProConfig import ProConfig
+
+class Development(Config):
+    LOGGER = True
+    
+    
+    API_HASH = "eaeb978f5221830d056f70d16542850d"
+    APP_ID = 1841354
+    
+    GITHUB_ACCESS_TOKEN = "f8fbda0a6bb10b3f1435acc2af1a3f6e53785b55"
+    HEROKU_API_KEY = "6f00efee-c72d-469b-b325-a72641e792d7"
+    PLUGIN_CHANNEL = "-1001162862605"
+    PRIVATE_GROUP_ID = "-1001162862605"
+    STRING_SESSION = "1BJWap1sBu6z4O-5AB7w9ihp2nntolZWz5AE_Lla7ioGeDVI0O10L4Q5qXXz4EU5DMjJYgrvBH7lrHdR_dWngIEf-gGvmRK8f5C6v_rwzJljMZFMt8d10K1pB7jfUPlTaK3qc7T4kPF9Xqdhe122bmoPIfvDOjpCyxVONGEFDL9mWKrSAEv1xi41k32pdJlWXBwTikVkgZ80eQKEKcuLUWqMXREFInBQNyH_Ww8dvQonm_bM8DkYkjzV3YresbBMvle7Cwf77rPSLF84RGYo-kfF8bAolE9PiQ3FXo8YYLsbG3G17wqjCg9uQTlbS65a6EuyyPTMhPZMP-cCgIyk2b3eplW1MUgM="
+    TG_BOT_TOKEN = "1426909138:AAGVB5I1yjZAGHXw6rIY0l4D2T9nQ6MymAw"
+    TG_BOT_USER_NAME = "Modifiedbot"
+    
+    
+    
+    
+    CASH_API_KEY = None # Get one from https://www.alphavantage.co/support/#api-key
+    
+    # Get one from https://timezonedb.com/register
+    TIME_API_KEY = "G9ZYSW6CBXNM"
+
+    # OpenWeather Map API Key for .weather command
+    # Get from https://openweathermap.org/
+    OPEN_WEATHER_MAP_APPID = "c6b206cf6e120b84d4cd406d7bed14ab"
+    WEATHER_DEFCITY = "Accra"
+    WEATHER_DEFLANG = "en"
+
+
+    # Coffeehouse chatbot api key, get one from  https://coffeehouse.intellivoid.net/
+    LYDIA_API_KEY = "0119889999f91228cee4c9ac32d8555f8490ab4c23105ef5e718984be1747c2d82b6a03308ba148767428ac14e82fc7027eb467d9c6eae5e9419cc3e8b358159"
+
+    # Get one from https://wall.alphacoders.com/api.php
+    WALL_API = "14e0e09f171bf935ddbf03a3ea644dfb"
+
+    #Get from https://www.remove.bg/ ☚  
+    REM_BG_API_KEY = "afBSBm7ko1emKapEFm6KeDN7"
+
+    # Get from https://ocr.space/ocrapi
+    OCR_SPACE_API_KEY = "d2ab7c20e988957"
+
+    # for google photo https://j.mp/39lWQQm
+    # Google Drive Credentials 
+    # Get from https://console.cloud.google.com
+    G_DRIVE_CLIENT_ID = "537361281969-85j620r8q5ouo38jojs1b0p9uck9vt34.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "wZzlP0OzrUFn0gCR016GyGbr"
+    G_DRIVE_AUTH_TOKEN_DATA = ""
+    TEMP_DOWNLOAD_DIRECTORY = ""
+
+    # You have to have your own unique two values for API_KEY and API_SECRET
+    # Obtain yours from https://www.last.fm/api/account/create for Last.fm
+    LASTFM_API = "2ae66f9e68cbe669efb1baa5ffe393ab"
+    LASTFM_SECRET = "4ecd6f2f23c5a53748e6a156c95b11e7"
+    LASTFM_USERNAME = "GbeshMod"
+    LASTFM_PASSWORD = "omoigberale1."
+
+
+    # Wolfram api
+    # Get an API KEY from products.wolframalpha.com/api/
+    WOLFRAM_API = "U8XPT7-WPJ6PJELL3"
+
+    # Genius lyrics get this value from https://genius.com/developers both has
+    # same values
+    # Client Access Token from https://genius.com/api-clients
+    GENIUS_CLIENT_ID = "sYN_gI-Ca1uSLpa6TeJuucoTqlDVdh5lm_k0vK1uCjxp3ssBHjJqWb_7T0D_TOHY"
+    GENIUS_ACCESS_TOKEN = "zJy2htM5GguF3C9JGalCSxg6OxuklGbEeANK0G6jmf2ezuIZN7Xzm6Gmm0_CNF-e"
+    GENIUS_CLIENT_SECRET = "r5iIUWNVR-1UmD0oXTRzmdA2YFfNl2A471s1Ez_3ghTcIgnbZiVhoIug6yNkicH-MpVRDXv1iHMUHmOKwpOr6A"
+
+    # API for direct link uptobox, https://docs.uptobox.com/#how-to-find-my-api-token
+    USR_TOKEN_UPTOBOX = "e5a8f5612fb3d4ee9781ab08bd62b3a781lhs"
+    #https://uptobox.com/affiliate?aff_id=13511425
+    
+    
+    
+    # This is required for the speech to text module. Get your USERNAME from https://console.bluemix.net/docs/services/speech-to-text/getting-started.html
+    IBM_WATSON_CRED_URL = "HlshyR2Lx8"
+    IBM_WATSON_CRED_PASSWORD = "Omoigberale1."
+
+    # Deepai value can get from https://deepai.org/
+    DEEP_API_KEY = "caea9036-57e1-4db4-8038-a093d282177e"
+
+    # Get your own ACCESS_KEY from http://api.screenshotlayer.com/api/capture
+     SCREEN_SHOT_LAYER_ACCESS_KEY = "91872032d61d8b8f03bf9de5f9b56e84"
+
+    # Need to get an API key from https://rapidapi.com/api-sports/api/api-football-beta
+    FOOTBALL_API_KEY = ""
+
+    # Need to get an API key from https://gplinks.in
+    GPLINKS_API_KEY = "32e9cb168c722bc25f8c7672c6943998860e47c5"
+
+    # Need to get an API key from https://virustotal.com
+    VIRUSTOTAL_API_KEY = "f8bac67b0d09a3c2ca697ab9c69b1e0949d1750300c4f31c4793b09b5b8d6801"
+
+# class Production(Config):
+#     LOGGER = True
