766058e3287fa5e4adc498d7a5c1bf4d84c7b9d8
Gbesh <63529893+GbeshMod@users.noreply.github.com>
2021-02-23 14:44:11 +0000

    Add files via upload

--- modified/Configs.py
+++ modified/Configs.py
@@ -159,9 +159,7 @@ class Config(object):
     NO_SONGS = bool(os.environ.get("NO_SONGS", False))
     MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 3))
     NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", False))
-    PM_LOGGR_BOT_API_ID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
-    if PM_LOGGR_BOT_API_ID:
-    PM_LOGGR_BOT_API_ID = int(PRIVATE_GROUP_ID)
+    PM_LOGGR_BOT_API_ID = os.environ.get("PRIVATE_GROUP_ID", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", "\.")
     SUDO_COMMAND_HAND_LER = os.environ.get("SUDO_COMMAND_HAND_LER", "\.")
